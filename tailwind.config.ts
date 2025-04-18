c65403c3434dd39feb95283c32952a90f73e3a9a
clry2002 <clry20@gmail.com>
2025-04-18 18:15:05 +0800

    remodified tailwind css codes to the right parts of the codes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,14 +16,18 @@
 // tailwind.config.js
 
 // Revised for deployment purpose
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+export default config;
