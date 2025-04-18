0e17dcb1a211bd38423ab280a8c1863175515fd1
clry2002 <clry20@gmail.com>
2025-04-18 20:41:25 +0800

    installed @tailwindcss/postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -5,20 +5,11 @@
 // export default config;
 
 // Modify for deployment purposes
-// postcss.config.js
-// postcss.config.mjs
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
 
-// Create __dirname equivalent for ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// With nesting plugin
 const config = {
   plugins: {
     'postcss-nesting': {},
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
