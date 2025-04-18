b45ee0f2445a74000dd913b7b026d2f59a6aeaba
clry2002 <clry20@gmail.com>
2025-04-18 20:32:04 +0800

    installed postcss nesting pkg

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -14,12 +14,11 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// With nesting plugin
 const config = {
   plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {
-      config: join(__dirname, 'tailwind.config.ts')
-    },
+    'postcss-nesting': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
